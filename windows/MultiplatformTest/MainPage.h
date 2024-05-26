ff9815036527528fe0ef59e3f9b9d5af2780318f
Joshua Siwek <joshua.siwek@jaemacom.de>
2024-05-26 15:45:18 +0200

    init project; added RN Packages to run on Windows and MacOS

---
+++ windows/MultiplatformTest/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MultiplatformTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MultiplatformTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
